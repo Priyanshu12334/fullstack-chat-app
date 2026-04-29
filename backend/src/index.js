7ce2893b8fe84d3df250eecaf932ea2a622d49c8
Priyanshu Suyal <suyalpriyanshu2@gmail.com>
2026-04-29 19:12:34 +0530

    fix error

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,7 +13,6 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
  
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -37,13 +35,6 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
